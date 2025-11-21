17ed00ead088fb06b0a91322bd88f8f9b469c105
FIGUEIRAS Jossua <jossufigueiras@icloud.com>
2025-11-21 01:08:59 +0100

    first commit

---
+++ client/src/components/Home.tsx
@@ -0,0 +1,21 @@
+function Home() {
+	
+	return (
+		<div className="max-w-xl mx-auto flex flex-col gap-6 items-center justify-center min-h-screen">
+			<a
+				href="https://www.smartworldsolutions.tech/"
+				target="_blank"
+				rel="noopener"
+			>
+				<img
+					src="/sws.png"
+					className="w-[200px] mb-10 cursor-pointer"
+					alt="SWS logo"
+				/>
+			</a>
+			<h1 className="text-5xl font-black">Nuit de l'info 2025</h1>
+		</div>
+	);
+}
+
+export default Home;
